d2a99b33ae469ead421706738cb5c727bfda4b5e
Matti Salokangas <matti.salokangas@gmail.com>
2016-01-13 16:15:47 -0800

    Allow links in note body

--- BlocNotes/DetailViewController.m
+++ BlocNotes/DetailViewController.m
@@ -8,10 +8,12 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController () <UIGestureRecognizerDelegate>
+@interface DetailViewController () <UIGestureRecognizerDelegate, UITextViewDelegate>
 
 @property (strong, nonatomic) UILongPressGestureRecognizer *longGestureRecognizerForTitle;
 @property (strong, nonatomic) UILongPressGestureRecognizer *longGestureRecognizerForBody;
+@property (strong, nonatomic) UITapGestureRecognizer *tapGestureRecognizerForBody;
+@property (strong, nonatomic) UITapGestureRecognizer *tapGestureRecognizer;
 //@todo There is a better way...
 @property (strong, nonatomic) NSString *originalDetailItemTitle;
 @property (strong, nonatomic) NSString *originalDetailItemBody;
@@ -20,21 +22,26 @@
 
 @implementation DetailViewController
 
-#pragma mark - Managing the detail item
+static NSString *titleKey = @"title";
+static NSString *bodyKey = @"body";
+static NSString *modifiedOnKey = @"modifiedOn";
+static NSString *createdOnKey = @"createdOn";
+static NSString *preciseFormatString = @"MM/dd/yyyy h:mm a";
+static NSString *recentFormatString = @"E h:mm a";
 
-- (void)setDetailItem:(id)newDetailItem {
-    if (_detailItem != newDetailItem) {
-        _detailItem = newDetailItem;
-        
-        self.originalDetailItemTitle = [[self.detailItem valueForKey:@"title"] description];
-        self.originalDetailItemBody = [[self.detailItem valueForKey:@"body"] description];
-            
-        // Update the view.
-        [self configureView];
-    }
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self configureView];
+    
+    self.noteBodyField.delegate = self;
+    self.noteBodyField.selectable = YES;
+    [self toggleEditMode:NO];
 }
 
-- (void)configureView {
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
     self.longGestureRecognizerForTitle = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(shareTitle:)];
     self.longGestureRecognizerForTitle.delegate = self;
     [self.noteTitleField addGestureRecognizer:self.longGestureRecognizerForTitle];
@@ -43,26 +50,49 @@
     self.longGestureRecognizerForBody.delegate = self;
     [self.noteBodyField addGestureRecognizer:self.longGestureRecognizerForBody];
     
+    self.tapGestureRecognizerForBody = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tappedBodyText:)];
+    [self.noteBodyField addGestureRecognizer:self.tapGestureRecognizerForBody];
+    self.tapGestureRecognizerForBody.delegate = self;
+    self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapFired:)];
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    [self.detailItem setValue:self.noteTitleField.text forKey:titleKey];
+    [self.detailItem setValue:self.noteBodyField.text forKey:bodyKey];
+    
+    if ([self hasChanges]) {
+        [self.detailItem setValue:[NSDate date] forKey:modifiedOnKey];
+        [self.delegate needsSaving];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)configureView {
     // Update the user interface for the detail item.
     if (self.detailItem) {
-        self.noteTitleField.text = [[self.detailItem valueForKey:@"title"] description];
-        self.noteBodyField.text = [[self.detailItem valueForKey:@"body"] description];
+        self.noteTitleField.text = [[self.detailItem valueForKey:titleKey] description];
+        self.noteBodyField.text = [[self.detailItem valueForKey:bodyKey] description];
         
-        NSDate *createdOn = [self.detailItem  valueForKey:@"createdOn"];
-        NSDate *modifiedOn = [self.detailItem valueForKey:@"modifiedOn"];
+        NSDate *createdOn = [self.detailItem  valueForKey:createdOnKey];
+        NSDate *modifiedOn = [self.detailItem valueForKey:modifiedOnKey];
         
         NSDateFormatter *createdOnFormatter = [[NSDateFormatter alloc] init];
         NSDateFormatter *modifiedOnFormatter = [[NSDateFormatter alloc] init];
-        
-        NSString *preciseFormatString = @"MM/dd/yyyy h:mm a";
-        NSString *recentFormatString = @"E h:mm a";
         
         NSString *createdOnFormatString = preciseFormatString;
         NSString *modifiedOnFormatString = preciseFormatString;
         
         int daysSinceCreated = [self daysFromToday:createdOn];
         int daysSinceModified = [self daysFromToday:modifiedOn];
-
+        
         if (daysSinceCreated < 8) {
             createdOnFormatString = recentFormatString;
         }
@@ -79,30 +109,18 @@
     }
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    self.automaticallyAdjustsScrollViewInsets = NO;
-    
-    [self configureView];
-}
+#pragma mark - Managing the detail item
 
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-    
-    [self.detailItem setValue:self.noteTitleField.text forKey:@"title"];
-    [self.detailItem setValue:self.noteBodyField.text forKey:@"body"];
-    
-    if ([self hasChanges]) {
-        [self.detailItem setValue:[NSDate date] forKey:@"modifiedOn"];
-        [self.delegate needsSaving];
+- (void)setDetailItem:(id)newDetailItem {
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+        
+        self.originalDetailItemTitle = [[self.detailItem valueForKey:titleKey] description];
+        self.originalDetailItemBody = [[self.detailItem valueForKey:bodyKey] description];
+            
+        // Update the view.
+        [self configureView];
     }
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 # pragma mark Gestures
@@ -113,14 +131,33 @@
 
 - (void) shareTitle:(UILongPressGestureRecognizer *)sender {
     if (sender.state == UIGestureRecognizerStateBegan) {
-        [self showSharingMenu:@[[self.detailItem valueForKey:@"title"]]];
+        [self showSharingMenu:@[[self.detailItem valueForKey:titleKey]]];
     }
 }
 
 - (void) shareBody:(UILongPressGestureRecognizer *)sender {
     if (sender.state == UIGestureRecognizerStateBegan) {
-        [self showSharingMenu:@[[self.detailItem valueForKey:@"body"]]];
+        [self showSharingMenu:@[[self.detailItem valueForKey:bodyKey]]];
     }
+}
+
+- (void) tappedBodyText:(UITapGestureRecognizer *)sender {
+    [self toggleEditMode:YES];
+    [self.noteBodyField becomeFirstResponder];
+}
+
+- (void) tapFired:(UITapGestureRecognizer *)sender {
+    [self.view endEditing:YES];
+}
+
+# pragma mark UITextView Delegate
+
+-(void)textViewDidEndEditing:(UITextView *)textView {
+    [self toggleEditMode:NO];
+}
+
+-(BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange {
+    return YES;
 }
 
 # pragma mark Privates
@@ -139,15 +176,26 @@
     NSInteger pastDate=[calendar ordinalityOfUnit:NSCalendarUnitDay
                                          inUnit:NSCalendarUnitEra
                                         forDate:date];
-    return abs(pastDate - today);
+    return abs((int)pastDate - (int)today);
 }
 
 - (BOOL) hasChanges {
-    NSString *title = [[self.detailItem valueForKey:@"title"] description];
-    NSString *body = [[self.detailItem valueForKey:@"body"] description];
+    NSString *title = [[self.detailItem valueForKey:titleKey] description];
+    NSString *body = [[self.detailItem valueForKey:bodyKey] description];
     
     return ![title isEqualToString:self.originalDetailItemTitle]
     || ![body isEqualToString:self.originalDetailItemBody];
+}
+
+- (void) toggleEditMode:(BOOL)on {
+    if (on == YES) {
+        self.noteBodyField.editable = YES;
+        self.noteBodyField.dataDetectorTypes = UIDataDetectorTypeNone;
+    }
+    else {
+        self.noteBodyField.editable = NO;
+        self.noteBodyField.dataDetectorTypes = UIDataDetectorTypeAll;
+    }
 }
 
 @end
